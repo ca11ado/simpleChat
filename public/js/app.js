b11f795629c0f8a070cc607b40253e6aee7c7072
tos <t00s@ya.ru>
2015-11-24 19:01:28 +0300

    Эксперименты с Promises

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@
  * Created by tos on 06.11.2015.
  */
 
+'use strict';
+
+
 let SChatWebSocketComponent = require('./components/SChatWebSocketComponent'),
     SChatComponent = require('./components/SChatComponent'),
     SChatActions = require('./actions/SChatActions');
@@ -9,3 +12,27 @@ let SChatWebSocketComponent = require('./components/SChatWebSocketComponent'),
 console.log('starting app');
 
 SChatActions.connectToWS('ws://localhost:8080');
+
+function httpGet(url) {
+    return new Promise(function(resolve){
+      setTimeout(function(){
+          resolve(url);
+      }, 100);
+    });
+}
+
+let urls = ['guest.json', 'user.json'];
+
+// ... ваш код
+let results = [];
+let all = urls.reduce((prev, current) => {
+    return prev.then(result => {
+        if (result) results.push(result);
+        return httpGet(current);
+    });
+}, Promise.resolve());
+
+//all.then(console.log(results));
+
+
+Promise.resolve().then(httpGet('test1')).then(result => httpGet(result + 'url2')).then(result => console.log(result));
